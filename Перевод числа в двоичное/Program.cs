dbe72e713d3abfaf23be9c3700f68b5191081f3a
Aleksandr <lexx-j@mail.ru>
2023-09-02 18:18:40 +0300

    Перевод числа в двоичное

---
+++ Перевод числа в двоичное/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int n = a, i = 0;
+
+while (a >= 1)
+{
+    a = a / 2;
+    i++;
+}
+int[] array = new int[i];
+while (i >= 1)
+{
+    array[i - 1] = n % 2;
+    n = n / 2;
+    i--;
+}
+for (i = 0; i < array.Length; i++)
+    Console.Write(array[i]);
